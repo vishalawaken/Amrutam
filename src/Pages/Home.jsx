7879a4233656fbfae4a05caf323f56bc5f2cc927
vishalawaken <tsinghvishall@gmail.com>
2025-08-22 09:45:36 +0530

    Refactor Home component for improved code readability and consistency in import statements. Ensure proper formatting and structure in the component's return statement.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Search_Bar from '../components/Search_Bar'
-import Product_Carousal from '../components/Product_Carousal'
-import Categories from '../components/categories'
-import Play_Store from '../components/Play_Store'
-
+import React from "react";
+import Navbar from "../components/Navbar";
+import Search_Bar from "../components/Search_Bar";
+import Product_Carousal from "../components/Product_Carousal";
+import Categories from "../components/Categories";
+import Play_Store from "../components/Play_Store";
 
 const Home = () => {
   return (
     <div>
-        
-        <Product_Carousal></Product_Carousal>
-        
+      <Product_Carousal></Product_Carousal>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
